b6a23be200f855a2cb637514f39d6fcf003e4a5c
Nick Shumilov <cjshmelle@yandex.ru>
2025-01-26 21:26:49 +0300

    admin_zone cerated and fully translated

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -1,3 +1,9 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from .models import Category
+from .models import Post
+from .models import Location
+
+admin.site.register(Category)
+admin.site.register(Post)
+admin.site.register(Location)
