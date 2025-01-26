a4d5bbfd91fafb42d3b7bb992cceec02b720f5e9
Nick Shumilov <cjshmelle@yandex.ru>
2025-01-26 21:18:43 +0300

    fixture_loading_success

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -1,3 +1,89 @@
-# from django.db import models
+from django.db import models
+# from core.models import PublishedModel
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Location(models.Model):
+    name = models.CharField('Название места', max_length=256)
+    is_published = models.BooleanField('Опубликовано', default=True)
+    created_at = models.DateTimeField('Добавлено', auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'местоположение'
+        verbose_name_plural = 'Местоположения'
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    title = models.CharField('Заголовок', max_length=256)
+    description = models.TextField('Описание', )
+    slug = models.SlugField(
+        'Идентификатор',
+        unique=True,
+        help_text='Идентификатор страницы для URL; '
+                  'разрешены символы латиницы, цифры, дефис '
+                  'и подчёркивание.'
+    )
+    is_published = models.BooleanField(
+        'Опубликовано',
+        default=True,
+        help_text='Снимите галочку, чтобы скрыть публикацию.'
+    )
+    created_at = models.DateTimeField('Добавлено', auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='post',
+        verbose_name='Категория',
+        help_text='Укажите категорию для публикации.'
+    )
+    is_published = models.BooleanField(
+        'Опубликовано',
+        default=True,
+        help_text='Снимите галочку, чтобы скрыть публикацию.'
+    )
+    created_at = models.DateTimeField('Добавлено', auto_now_add=True)
+    title = models.CharField('Заголовок', max_length=256)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Автор публикации'
+    )
+    text = models.TextField('Текст')
+    pub_date = models.DateTimeField(
+        'Дата и время публикации',
+        help_text='Если установить дату и время в'
+        ' будущем — можно делать отложенные публикации.'
+    )
+    location = models.ForeignKey(
+        Location,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name='Местоположение',
+        help_text='Укажите местоположение '
+        'публикации.'
+    )
+
+    class Meta:
+        verbose_name = 'публикация'
+        verbose_name_plural = 'Публикации'
+
+    def __str__(self):
+        return self.title
